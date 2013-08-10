84b492bbb6f08680378204543aa7ba6a2d4d19fc
Paolo Carrasco <paolo.carrasco@gmail.com>
2013-08-10 16:12:14 -0400

    setting the result status codes, check the existance of order on GET

--- app.js
+++ app.js
@@ -19,8 +19,15 @@ app
         res.json(resources);
     }) 
     .get('/orders/:id', function(req, res) {
-        var order = orders[req.params.id];
-        res.json(order);
+        var orderId = req.params.id;
+        if(orderId in orders) {
+            var order = orders[orderId];
+            res.json(order);
+        }
+        else {
+            res.status(404);
+        }
+        res.end()
     })
     .get('/orders', function(req, res) {
         res.json(orders);
@@ -28,7 +35,9 @@ app
     .post('/orders', function(req, res) {
         var order = req.body;
         orders.push(order);
-        res.end();
+        res
+            .status(201)
+            .end();
     });
     
 app.listen(process.env.PORT || 8080);
